b2d1a3d67db1159df810fd0c234bc7a67575c9b2
tanjil-hosain <tanjilh85@gmail.com>
2026-06-27 23:13:57 +0600

    user_seeder data load

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+            User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456'),
+            'role' => 'admin'
+        ]);
+         User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('123456'),
+            'role' => 'user'
+        ]);
     }
 }
