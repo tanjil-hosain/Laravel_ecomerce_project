634538101336643b65da3f72576fa1035ad8fd4b
tanjil-hosain <tanjilh85@gmail.com>
2026-06-30 00:07:24 +0600

    sub_category routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Sub_category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,6 +23,7 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 
     Route::resource('category',CategoryController::class);
+    Route::resource('sub_category', Sub_category::class);
 });
 
 require __DIR__.'/auth.php';
