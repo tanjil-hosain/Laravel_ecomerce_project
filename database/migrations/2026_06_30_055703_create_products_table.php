9f462108b05c79760d6d0921aa9f2a7ef3c48171
tanjil-hosain <tanjilh85@gmail.com>
2026-06-30 19:41:02 +0600

    products migration

--- database/migrations/2026_06_30_055703_create_products_table.php
+++ database/migrations/2026_06_30_055703_create_products_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name',100);
+            $table->string('description',500)->nullable();
+            $table->string('sku',50);
+            $table->double('price');
+            $table->double('buying_price');
+            $table->tinyInteger('stock');
+            $table->string('image',100)->nullable();
+            $table->tinyInteger('category_id');
+            $table->tinyInteger('sub_category_id');
+            $table->tinyInteger('unit_id');
+
             $table->timestamps();
         });
     }
